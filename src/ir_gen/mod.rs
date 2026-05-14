3a1ff97e3a85ed608d721b23c1570c7c2bf9a4ea
xiaoli-white <xiaolitongxue-all@outlook.com>
2026-05-14 22:08:08 +0800

    ✨ feat(ir_gen): Refactor IRGenerator to use context reference and streamline module generation

--- src/ir_gen/mod.rs
+++ src/ir_gen/mod.rs
@@ -1,72 +1,57 @@
-use std::{cell::RefCell, ops::Deref, rc::Rc};
+use std::{cell::RefCell, rc::Rc};
 
 use anyhow::{Ok, Result};
-use inkwell::{
-    builder::Builder,
-    context::Context,
-    module::Module,
-    types::{AnyType, BasicType, BasicTypeEnum, FunctionType},
-};
+use inkwell::{builder::Builder, context::Context, module::Module, types::AnyType};
 
 use crate::{
     ast::{node::Program, statement::Statement},
     types::Type,
 };
 
-pub struct IRGenerator {
-    context: Context,
+pub struct IRGenerator<'ctx> {
+    context: &'ctx Context,
+    module: Module<'ctx>,
+    #[allow(dead_code)]
+    builder: Builder<'ctx>,
 }
 
-impl IRGenerator {
-    pub fn new() -> Self {
+impl<'ctx> IRGenerator<'ctx> {
+    pub fn new(context: &'ctx Context) -> Self {
+        let module = context.create_module("main");
+        let builder = context.create_builder();
         Self {
-            context: Context::create(),
+            context,
+            module,
+            builder,
         }
     }
-    pub fn generate(&self, program: &Program) -> Result<Module> {
-        let ir_module = self.context.create_module("main");
-        let builder = self.context.create_builder();
+
+    pub fn generate(&'ctx self, program: &Program) -> Result<Module<'ctx>> {
         for stmt in &program.statements {
-            self.resolve_statement(&ir_module, &builder, stmt.clone())?;
+            self.resolve_statement(stmt.clone())?;
         }
-        Ok(ir_module)
+        Ok(self.module.clone())
     }
-    fn resolve_statement(
-        &self,
-        ir_module: &Module,
-        builder: &Builder,
-        statement: Rc<RefCell<Statement>>,
-    ) -> Result<()> {
+
+    fn resolve_statement(&'ctx self, statement: Rc<RefCell<Statement>>) -> Result<()> {
         match &*statement.borrow() {
-            Statement::VariableDecl {
-                name,
-                type_expression,
-                initializer,
-                ty,
-                ..
-            } => {}
-            Statement::FunctionDecl {
-                name,
-                parameters,
-                return_type,
-                body,
-                ty: Some(ty),
-                ..
-            } => {
-                if let Type::Function(parameter_types, ret_type) = &*ty.borrow() {
+            Statement::VariableDecl { .. } => {}
+            Statement::FunctionDecl { ty: Some(ty), .. } => {
+                if let Type::Function(parameter_types, _ret_type) = &*ty.borrow() {
                     let mut param_types = Vec::new();
                     for param_type in parameter_types {
                         param_types.push(self.resolve_type(param_type.clone())?);
                     }
                 }
             }
-            Statement::Return { value: Some(value) } => {}
-            Statement::ExpressionStatement { expression } => {}
+            Statement::Return { value: Some(_) } => {}
+            Statement::ExpressionStatement { expression: _ } => {}
             _ => {}
         }
         Ok(())
     }
-    fn resolve_type<'a>(&'a self, ty: Rc<RefCell<Type>>) -> Result<Box<dyn AnyType<'a> + 'a>> {
-        Ok(Box::new(self.context.i64_type()))
+
+    fn resolve_type(&'ctx self, _ty: Rc<RefCell<Type>>) -> Result<Box<dyn AnyType<'ctx> + 'ctx>> {
+        Ok(Box::new(self.context.i64_type()) as Box<dyn AnyType<'ctx> + 'ctx>)
     }
 }
